5e982a67889ae4de4dade7433074c506e07f2479
Hecabrus <lolripper26@gmail.com>
2025-03-15 18:49:58 +0530

    Update config.js

--- src/services/database/config.js
+++ src/services/database/config.js
@@ -4,7 +4,7 @@ export const DATABASE_CONFIG = {
         apiKey: import.meta.env.VITE_WEAVIATE_API_KEY
     },
     SUPABASE: {
-        url: import.meta.env.VITE_SUPABASE_URL,
-        apiKey: import.meta.env.VITE_SUPABASE_API_KEY
+        url: 'https://ybmwyhyylqhkfgtspllm.supabase.co',
+        apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZi6InlibXd5aHl5bHFoa2ZndHNwbGxtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MTYxOTEyMSwiZXhwIjoyMDU3MTk1MTIxfQ.tkf5T8Bm42GjuUswOPEPFijN1VPTpN7sCCu66dn2Gfs'
     }
 };
