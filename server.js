9490bff426ca5e2ac19671d5fa615788e76b8dac
Hecabrus <lolripper26@gmail.com>
2025-03-15 17:53:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const app = express();
 // Serve static files from dist directory
 app.use(express.static('dist'));
 
-// API routes
-app.use('/api', require('./api/server.py'));
-
 // Serve frontend for all other routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
@@ -15,5 +12,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Frontend server running on port ${PORT}`);
 });
